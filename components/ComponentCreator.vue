cd3409cfcce6604b0514ecb2c5e061d6d948ccd1
Romain Cointet <marchlamute@gmail.com>
2020-12-17 12:35:51 +0100

    cours 0-5

--- components/ComponentCreator.vue
+++ components/ComponentCreator.vue
@@ -3,7 +3,7 @@
     <div v-for="(c, i) in content" :key="i">
         <CodeHighlighter :language="c.language" :code="c.value" :fileName="c.fileName" v-if="c.type==='code'"/>
         <nuxt-link :to="c.src" class="btn btn-primary cta" v-else-if="c.type==='nuxt-link'">{{ c.value }}</nuxt-link>
-        <img :src="require(`~/assets/img/${c.src}`)" :alt="c.alt" v-else-if="c.type==='img'"/>
+        <img :src="require(`~/assets/img/${c.src}`)" :class="c.class?c.class:''" :alt="c.alt" v-else-if="c.type==='img'"/>
         <a :href="c.href" :class="c.class" v-else-if="c.type==='a'">{{ c.value }}</a>
         <component v-else :is="c.type" v-html="c.value" :class="c.class?c.class:''"></component>
     </div>
